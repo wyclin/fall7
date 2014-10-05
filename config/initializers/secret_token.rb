3edaadf8ec3c1e041eaeaf2fe63657bd5d35f084
Wayne Lin <wayne@wyclin.com>
2014-10-05 05:01:19 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fall7::Application.config.secret_token = '2797fb678868baeb2a71ba18f39f3cba1d04c552a77b58a8ba3c3e054d01de4348b824a3529304defca015652e0fceca9dddb785e779376286a21f36eabccfe2'
